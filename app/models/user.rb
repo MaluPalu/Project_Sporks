2b41cb10a7bfbff837ec8eebe37a10ab5b0f2b92
Esther Weon <esther.s.weon@gmail.com>
2017-09-29 12:44:43 -0700

    Esther's code etiquette comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :recipes
+  # get rid of this comment – if you want, you can add all these modules for now
+  # and delete as you realize you do not need / want them
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
