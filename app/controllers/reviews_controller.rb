97f8497e280234362f5cdfbb3e058a7941e29cf7
Jevell Rollins <jkwr92@gmail.com>
2017-09-29 16:56:04 -0700

    review CRUD is finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,13 +6,38 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.find_by_id(params[:recipe_id])
     @review = Review.create(review_params)
-    redirect_to 
+    @recipe.reviews << @review
+    current_user.reviews << @review
+    redirect_to recipe_path(@recipe)
+  end
+
+  def show
+    @review = Review.find_by_id(params[:id])
+    @recipe = Recipe.find_by_id(params[:recipe_id])
+  end
+
+  def edit
+    @review = Review.find_by_id(params[:id])
+    @recipe = Recipe.find_by_id(params[:recipe_id])
+  end
+
+  def update
+    review = Review.find_by_id(params[:id])
+    review.update(review_params)
+    redirect_to recipe_review_path(params[:recipe_id], params[:id])
+  end
+
+  def destroy
+    @review = Review.find_by_id(params[:id])
+    @review.destroy
+    redirect_to user_reviews_path(@review.user_id, @review.id)
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:image, :content, :rating)
+    params.require(:review).permit(:image, :content, :rating, :title)
   end
 end
