2074ecfbc2c97b6cb07340e4ecd15d53dc5f57c7
MaluPalu <malu.peralta808@gmail.com>
2017-10-03 08:06:21 -0700

    following

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   has_many :favorite_recipes
   has_many :favorites, through: :favorite_recipes, source: :recipe
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
