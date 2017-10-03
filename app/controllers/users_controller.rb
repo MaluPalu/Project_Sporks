2bb6ce0799c7414007dbc40dbdc8966428d46318
conmart <connor.a.martinelli@gmail.com>
2017-10-03 13:24:36 -0700

    updated routes and added follower helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
+  before_action :get_user, except: [:index]
 
   def index
   end
 
   def show
-    @user = User.find_by_id(params[:id])
   end
 
   def follow
-    following_arr = current_user.followings.map { |x| x.id }
-    @user = User.find_by_id(params[:id])
+
     type = params[:type]
-    if type == "follow" && !following_arr.include?(@user.id)
+    if type == "follow" && !following_user?
       @user.followers << current_user
       redirect_to user_path(@user), notice: 'You followed ' + @user.name
 
-    elsif type == "unfollow"
+    elsif type == "unfollow" && following_user?
       current_user.followings.delete(@user)
       redirect_to user_path(@user), notice: 'Unfollowed ' + @user.name
 
@@ -26,22 +25,26 @@ class UsersController < ApplicationController
   end
 
   def recipes
-    @user = User.find_by_id(params[:user_id])
     @recipes = @user.recipes
   end
 
   def followings
-    @user = User.find_by_id(params[:user_id])
     @follows = @user.followings
   end
 
   def reviews
-    @user = User.find_by_id(params[:user_id])
     @reviews = @user.reviews
   end
 
   def favorites
-    @user = User.find_by_id(params[:user_id])
     @recipes = @user.favorites
   end
+
+  private
+
+  def get_user
+    @user = User.find_by_id(params[:id])
+  end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
     fav_arr.include?(params[:id].to_i)
   end
 
+  def following_user?
+    following_arr = current_user.followings.map { |x| x.id }
+    following_arr.include?(params[:id].to_i)
+  end
+
 end
