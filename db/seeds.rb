6a6912012740069f7fa0ec448301931cf2d97b49
conmart <connor.a.martinelli@gmail.com>
2017-10-04 16:08:50 -0700

    seed now destroys everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Recipe.create ({title: 'Garlic Chicken', description: 'Soul food', prep_time: '30min', cook_time: '1hr', ease: 'Medium',
- ingredients: 'chicken, psta, milk, pepper, sale, onions, garlic', instructions: 'leave overnight', serves: '3', photo: 'none' })
-
-
-
-
-
-
-
-
-
-
-
-
+User.destroy_all
+Recipe.destroy_all
+Review.destroy_all
