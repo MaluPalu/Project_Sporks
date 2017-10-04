613b6527f99a868bbd41f874635a71238a3ef824
conmart <connor.a.martinelli@gmail.com>
2017-10-03 17:38:49 -0700

    fixed another route issue

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :get_recipe, only: [:favorite, :show, :edit, :udpate, :destroy]
+  before_action :get_recipe, only: [:favorite, :show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
